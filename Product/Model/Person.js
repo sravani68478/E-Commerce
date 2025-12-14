17934610f341243d4302ace358e8e36d6159d0b7
msravani <mutyalasravani727@gmail.com>
2025-12-14 11:35:53 +0530

    My Back end changes

---
+++ Product/Model/Person.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const perschema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  email: {
+    type: String,
+    match: [
+      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,4}$/,
+      "Please enter a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+  },
+});
+perschema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+module.exports = mongoose.model("Person", perschema);
---
+++ Product/Model/Product.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const productschema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+  },
+  discription: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Product", productschema);
